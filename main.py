a4d003e3ec8bc130f7b361662a523fe6c79d526b
kapustin4613 <kapustin4613@gmail.com>
2025-08-21 13:37:18 +0900

    home update

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 
 
+LOG_FILE = mission_computer_main.log
+
+def main():
+	print_log_file()
+
+	log_list = log_to_list()
+
+	print(log_list)
+
+	print(sorted(log_list, key=lambda x: x[0], reverse=True))
+
+	log_dict = list_to_dict(log_list)
+
+	save_json_file(log_dict)
+
 if __name__ == "__main__":
-	print("Hello Mars")
+	main()
